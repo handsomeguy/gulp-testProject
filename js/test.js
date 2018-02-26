28b1b2724625270b0621ea872b033b0d40e7803a
handsomeGuy <864221141@qq.com>
2018-02-26 22:05:58 +0800

    file upload

---
+++ js/test.js
@@ -0,0 +1,4 @@
+var a = document.getElementById('content')
+
+console.log(a.nodeType);
+alert('ok');alert('ok');alert('ok');alert('ok');alert('ok');alert('ok');
---
+++ test.js
@@ -0,0 +1,13 @@
+function haha() {
+	var a = 1;
+	return a;
+}
+
+var b =haha();
+console.log(b);
+
+c = function () {
+	console.log(2)
+}
+
+c++;
